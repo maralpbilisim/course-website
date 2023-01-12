f2938bf4b19338a35c30e9f650770ac1c82055b2
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2023-01-12 22:03:54 +0300

    site updated

--- js/main.js
+++ js/main.js
@@ -119,6 +119,26 @@ $(window).on('load', function() {
 		}
 		
 	});
-
+	$('#customers-testimonials').owlCarousel({
+		loop: true,
+		center: true,
+		items: 3,
+		margin: 0,
+		autoplay: true,
+		dots:true,
+		autoplayTimeout: 8500,
+		smartSpeed: 450,
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  768: {
+			items: 2
+		  },
+		  1170: {
+			items: 3
+		  }
+		}
+	});
 })(jQuery);
 
